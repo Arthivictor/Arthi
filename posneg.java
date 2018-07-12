520a6dc62cdc7e69e78967ecc539c84a844d7f9d
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-12 10:34:35 +0530

    Create posneg.java

---
+++ posneg.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int b=9;
+	 if(b>0)
+	 {
+	 	System.out.print("positive");
+	 }
+	else if(b<0)
+	 {
+	 	System.out.print("negative");
+	 }
+	 else 
+	 {
+	 	System.out.print("zero");
+	 }
+	}
+}
