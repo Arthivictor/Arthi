1143961cc4c82a1186117618f11f34b42204fc54
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 10:10:32 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,21 @@
+class numbers
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=5;
+int b=3;
+int c=9;
+if(a>b&&a>c)
+{
+System.out.print("a is largest");
+}
+else if(b>a&&b>c)
+{
+System.out.print("b is largest");
+}
+else
+{
+System.out.print("c is largest");
+}
+}
+}
