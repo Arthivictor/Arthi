a1cdb8b16138685b43f31d4be472248c95630c59
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 21:49:27 +0530

    Create middleodd.java

---
+++ middleodd.java
@@ -0,0 +1,16 @@
+class middleodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=6;
+int n=12;
+for(i=6;i<=n;i++)
+{
+if(n%2!=0)
+{
+System.out.print(i);
+
+	}
+}
+}
+}
