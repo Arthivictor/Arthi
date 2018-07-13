c20ccc40a29e1b44f1bf0d47b13ea0bfcc0f875f
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 20:52:16 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,17 @@
+class integer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a[]={1,2,3,4,5};
+int n=2;
+int m;
+int i;
+int sum=0;
+for(i=0;i<n;i++)
+{
+m=sum+a[i];
+
+
+	}
+}
+}
