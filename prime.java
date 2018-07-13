0ae21bde9f20808ffe6fff4037df6ab607e209cf
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 21:36:53 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,18 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=5;
+for(int i=2;i<=a;i++)
+{
+if(a%i!=0)
+{
+System.out.print("prime");
+	}
+else
+{
+System.out.print("not a prime");
+}
+}
+}
+}
