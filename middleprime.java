3f864fa5758c8f6cd5819078fceb98b6c4ac4022
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 22:01:45 +0530

    Create middleprime.java

---
+++ middleprime.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=2;
+int n=10;
+for(i=2;i<=10;i++)
+{
+if(i%2!=0)
+{
+System.out.print(i);
+	}
+}
+}
+}
