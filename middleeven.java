5e33e89f271b62bcb0d15091759d72b62c989990
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 21:58:12 +0530

    Create middleeven.java

---
+++ middleeven.java
@@ -0,0 +1,16 @@
+class middleeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=1;
+int n=10;
+for(i=1;i<=10;i++)
+{
+if(i%2==0)
+{
+System.out.print(i);
+}
+}
+}
+}
+
