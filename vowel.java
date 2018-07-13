789743ec0e29b160a782bd2d522641cef1c8d53d
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 10:24:56 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='e';
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+System.out.print(ch +"vowel");
+}
+else
+{
+System.out.print(ch +"consonant");
+	}
+}
+}
