bc0b518f490795d8bb5c87cf78c43f7fb4880ea0
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-13 09:59:40 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,15 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='a';
+		if(ch>='a'&&ch<='z'||ch>='A'&&ch<='Z')
+		{
+			System.out.print(ch +"alphabet");
+		}
+		else
+		{
+			System.out.print(ch +"not an alphabet");
+		}
+		}
+	}
