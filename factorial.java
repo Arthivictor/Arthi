223fc496f17f8195ad451c0c015d669667f56876
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-14 14:01:17 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=4;
+int i;
+int fact=1;
+if(n>0)
+{
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+	}
+}
+}
+}
+Success	#stdin #stdout
