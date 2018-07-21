96717226c9b8a9d8d73bbdb2779db048a79b7877
Arthivictor <41137326+Arthivictor@users.noreply.github.com>
2018-07-21 09:41:01 +0530

    Create stringreverse.java

---
+++ stringreverse.java
@@ -0,0 +1,11 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String s="aabbcc";
+	StringBuilder s1=new StringBuilder();
+	s1.append(s);
+	s1=s1.reverse();
+	System.out.println(s1);
+	}
+}
